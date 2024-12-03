3dda49063c34719e4e159ba94df7d606b3b9b118
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-12-03 17:42:42 -0500

    feat: implement brands view

--- src/app/admin/brands/page.jsx
+++ src/app/admin/brands/page.jsx
@@ -1,3 +1,22 @@
-export default function AdminBrandsPage() {
-  return <div>AdminBrandsPage</div>
+import MainContent from '@/app/admin/_components/MainContent'
+import BrandItem from '@/app/admin/brands/_components/BrandItem'
+
+const loadBrands = async () => {
+  const module = await import('@/../public/mocks/brands.json')
+  return module.brands
+}
+
+export default async function AdminBrandsPage() {
+  const brands = await loadBrands()
+  const items = brands.map((brand) => ({
+    key: brand.id,
+    name: brand.name,
+    description: brand.description
+  }))
+
+  return (
+    <>
+      <MainContent items={items} ItemComponent={BrandItem} />
+    </>
+  )
 }
