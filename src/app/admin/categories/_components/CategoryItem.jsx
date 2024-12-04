f6a471b018973e8581086e46ce9b113aafcce4b0
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-12-03 19:58:43 -0500

    refactor: delete old unusable prop

--- src/app/admin/categories/_components/CategoryItem.jsx
+++ src/app/admin/categories/_components/CategoryItem.jsx
@@ -3,9 +3,8 @@
 export default function CategoryItem({ item }) {
   return (
     <div className='flex items-center bg-gray-100 p-4 rounded-md shadow-sm'>
-      <div className='flex flex-col w-[100px] mr-4 '>
-        <span className='text-gray-800'>{item.title}</span>
-        <span className='text-gray-500'>{item.name.slice(0, 10)}</span>
+      <div className='flex flex-col w-[100px] mr-4 bg-orange-500 p-2 text-white rounded-md font-bold'>
+        <span>{item.name.slice(0, 10)}</span>
       </div>
       <div className='flex flex-col'>
         <span className='text-gray-800 text-sm'>
